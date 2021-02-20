ffed6dcc47232beddc7bb77bf3a0cd1166733f78
Valery Kokanov <illigrium@gmail.com>
2021-02-21 02:40:57 +0300

    Adding fibonacci

--- script.hs
+++ script.hs
@@ -2,6 +2,12 @@ factorial :: (Eq a, Num a, Ord a) => a -> a
 factorial n | n < 0 = 0
             | n == 1 = 1
             | otherwise = n * factorial (n - 1)
+            
+fibonacci :: Integer -> Integer
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n > 1 = fibonacci (n - 1) + fibonacci (n - 2)
+            | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
 
 sumMatch :: (Eq a, Num a, Ord a) => a -> a
 sumMatch n | n < 0 = 0
